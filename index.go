22375f3865dc393ca8dd13b37f5f11547d32c833
Vinícius <timelord1025@gmail.com>
2021-09-18 10:50:04 -0300

    first commit

---
+++ index.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func callatz(n int) int {
+	if n%2 == 0 {
+		return n / 2
+	} else {
+		return (n * 3) + 1
+	}
+}
+
+func callatzSeries(n int) int {
+	i := 0
+	// fmt.Printf("n = %d\n", n)
+	for n != 1 {
+		n = callatz(n)
+		i = i + 1
+	}
+	// fmt.Printf("iterations: %d\n", i)
+	// fmt.Println("------")
+	return n
+}
+
+func main() {
+	begin := time.Now()
+	var a [10000]int
+	for i := 0; i < 10000; i++ {
+		n := callatzSeries(i + 1)
+		a[i] = n
+	}
+	end := time.Now()
+	fmt.Println("Array: ", a)
+	fmt.Printf("Execution time: %dms", end.Sub(begin).Milliseconds())
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const { log } = console
+
+function callatz(n) {
+    if (n % 2 == 0) {
+        return n / 2;
+    } else {
+        return (n * 3) + 1
+    }
+}
+
+function callatzSeries(n) {
+    let i = 0;
+    // log(`n = ${n}`)
+    for (; n != 1; n = callatz(n), i++) {
+    }
+    //log(`iterations: ${i}`)
+    // log('-----')
+    return n
+}
+
+function main() {
+    const begin = new Date()
+    let arr = []
+    for (let i = 1; i < 10000; i++) {
+        n = callatzSeries(i)
+        arr.push(n)
+    }
+    const end = new Date()
+    log(`Array: `, arr)
+    log(`Execution time: ${end.getTime() - begin.getTime()}ms`)
+}
+
+main()
